8f91cd9bb7ea37d7ec1ea715f2d158766d164cad
santhosh1011 <sandytambasandy@gmail.com>
2022-09-07 12:11:06 +0530

    newloop

---
+++ src/org/sam/Loopp.java
@@ -0,0 +1,40 @@
+package org.sam;
+
+public class Loopp {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i=0; i<5;i++) {
+			for(int j=0; j<2;j++) {
+				for(int d=0;d<2;d++) {
+					
+					System.out.println(i);
+				}
+			
+				
+				
+			}
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
