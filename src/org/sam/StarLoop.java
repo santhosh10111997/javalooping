6d0322ba8b566b1ea5e8cf32dc3beb7bc3043560
santhosh1011 <sandytambasandy@gmail.com>
2022-09-07 12:48:49 +0530

    dhilip's

--- src/org/sam/StarLoop.java
+++ src/org/sam/StarLoop.java
@@ -5,18 +5,10 @@ import java.util.Scanner;
 public class StarLoop {
 
 	public static void main(String[] args) {
-		
-	
-		Scanner s= new Scanner(System.in);
-		
-		System.out.println("6");
-		int rows = s.nextInt();
-		System.out.println("6");
-		for(int f=rows;f>=0;f--) {
-			for(int g=0;g<=f;g++) {
-				System.out.print("*");
-			}
-			System.out.println();
+		System.out.println("test 1");
+		System.out.println("test 2");
+		System.out.println("test 3");
+		System.out.println("test 4");
 		}
 		
 		
